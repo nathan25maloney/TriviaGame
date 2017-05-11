148e8b568248b9b0c925d511a080aef5c9b09124
Nathan Maloney <Nathan Maloney>
2017-05-10 23:30:05 -0700

    starts and randomly chooses first question and the option buttons can tell if it is a correct or incorrect guess

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,41 +1,67 @@
 var firstTime = true;
 var answer = "";
+var intervalId;
+var number = 30;
+var correct = 0;
+var incorrect = 0;
 
 var choices = {
-	one: {Question: "Question 1", option1: "Option 1", option2: "Option 2", option3:"Option 3", option4:"Option 4", answer: "option3"},
+	one: {Question: "How many feet are in a yard", option1: "2", option2: "5", option3:"1", option4:"3", answer: "option4"},
+	two: {Question: "How many inches are in a foot", option1: "8", option2: "14", option3:"12", option4:"6", answer: "option3"}
+};
 
+function pickRandomProperty(obj) {
+    var result;
+    var count = 0;
+    for (var prop in obj)
+        if (Math.random() < 1/++count)
+           result = prop;
+    return result;
+}
+
+function build() {
+	var result = pickRandomProperty(choices);
+	for (var key in choices) {
+	
+		if (key === result){
+			for (var key in choices[result] ) {
+			    if (choices[result].hasOwnProperty(key)) {
+				    if (key === "Question") {
+				    	$("#start").text(choices[result].Question);
+				    	console.log(key + " -> " + choices[result][key]);
+				    } else if(key === "answer") {
+				    	answer = choices[result][key];
+				    	console.log(answer);
+				    } else {
+					    var letterBtn = $("<button>");
+				        letterBtn.attr("class","letter-button letter letter-button-color");
+				        letterBtn.attr("id",key);
+				        letterBtn.attr("data-letter",choices[result][key]);
+				        letterBtn.text(choices[result][key]);
+				        $('#questions').append(letterBtn);
+				        console.log(key + " -> " + choices[result][key]);
+				        var breakButton = $("<br><br>");
+				        $("#questions").append(breakButton);
+				    };
+			    };
+			};
+		} 
+	};
 };
 
 
-
-function build() {
-	
-	for (var key in choices.one) {
-	  if (choices.one.hasOwnProperty(key)) {
-	    if (key === "Question") {
-	    	$("#start").text(choices.one.Question);
-	    	console.log(key + " -> " + choices.one[key]);
-	    } else if(key === "answer") {
-	    	answer = choices.one[key];
-	    	console.log(answer);
-	    } else {
-		    var letterBtn = $("<button>");
-	        letterBtn.attr("class","letter-button letter letter-button-color");
-	        letterBtn.attr("id",key);
-	        letterBtn.attr("data-letter",choices.one[key]);
-	        letterBtn.text(choices.one[key]);
-	        $('#questions').append(letterBtn);
-	        console.log(key + " -> " + choices.one[key]);
-	        var breakButton = $("<br>");
-	        var ID = '"#'+ key +'"';
-	        $("#questions").append(breakButton);
-	    }
-	  }
-	}
-}
-
 function run() {
+	function decrement() {
+      number--;
+      $("#show-number").html("<h2>Time Remaining: " + number + "</h2>");
+      if (number === 0) {
+        stop();
+        $("#show-number").html("<h2>Time's Up!</h2>");
+      }
+    }
+
 	if (firstTime) {
+		intervalId = setInterval(decrement, 1000);
 		build();
 	}
 	
@@ -44,23 +70,52 @@ function run() {
 
 
 
+    //  The stop function
+function stop() {
+     clearInterval(intervalId);
+}
+
+function checkAnswer(ans) {
+	console.log(answer);
+	console.log(ans);
+	if(answer === ans){
+		stop();
+		correct += 1;
+		console.log(correct);
+	} else {
+		incorrect += 1;
+		console.log(incorrect);
+		console.log("wrong answer");
+	}
+}
+
+
+
+
+
 
 $("#start").on("click", function() {
 	run();
 	firstTime = false;
+
+	$("#option1").on("click", function() {
+		checkAnswer("option1");
+	})
+
+	$("#option2").on("click", function() {
+		checkAnswer("option2");
+	})
+
+	$("#option3").on("click", function() {
+		checkAnswer("option3");
+	})
+
+	$("#option4").on("click", function() {
+		checkAnswer("option4");
+	})
+
 })
 
 
 
-$("#option1").on("click", function() {
-	console.log("test1");
-})
-$("#option2").on("click", function() {
-	console.log("test1");
-})
-$("#option3").on("click", function() {
-	console.log("test1");
-})
-$("#option4").on("click", function() {
-	console.log("test1");
-})
+	
